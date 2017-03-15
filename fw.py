e9d5e7465020d56d95f9e79400cba4fa3caca466
tyrone.lagore <tyrone.lagore@zone47-wc.cs.ucalgary.ca>
2017-03-15 12:53:23 -0600

    added Rule class and a RuleException class. Rule defines a single firewall rule and RuleException is raised if Rule receives bad values

--- fw.py
+++ fw.py
@@ -2,5 +2,26 @@
 
 
 class Rule:
+    """  """
     def __init__(self, direction, action, ip, port, flag):
+        self._direction = direction
+
+        if self._direction != 'in' && self._direction != 'out':
+            raise RuleException("Rule object field 'direction' must be either 'in' or 'out'")
         
+        self._action = action
+
+        if self._action != 'accept' && self._action != 'deny' && self._action != 'drop':
+            raise RuleException("Rule object field 'action' must be either 'accept', 'deny', or 'drop'")
+        
+        self._ip = ip
+        self._port = port
+        self._flag = flag
+
+
+class RuleException(Exception):
+    def __init__(self, msg):
+        self._msg = msg
+
+    def __str__(self):
+        return repr(self._message):
