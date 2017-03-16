0de2fe95fab5744f4f7b5d996cd29dc73be080dc
James MacIsaac <james.macisaac@zone47-wd.cs.ucalgary.ca>
2017-03-15 19:11:23 -0600

    Now parses packets and prints the response of the firewall

--- fw.py
+++ fw.py
@@ -59,17 +59,67 @@ class RuleManager:
         
         return rule
 
-    @staticmethod
-    def ipToLong(ip):
-        """ takes in a string format ip and converts it to a long int """
-        packedIP = socket.inet_aton(ip)
-        return struct.unpack("!L", packedIP)[0]
+    def parsePacket(self, packet):
+        """ 
+        checks to see if a packet conforms to any rules and returns the rule number.
+        returns -1 if no rule found
+        """
+        
+        pcktParts = packet.split()
+        
+        # needs exactly 4 parts
+        if len(pcktParts) != 4:
+            return None
+        
+        direction = pcktParts[0]
+        ip = pcktParts[1]
+        port = pcktParts[2]
+        flag = pcktParts[3]
 
-    @staticmethod
-    def longToIp(longIp):
-        """ takes in a long format ip and converts it to a string """
-        stringIp = socket.inet_ntoa(struct.pack("!L", longIp))
-        return stringIp
+        try:
+            pckt = Packet(direction, ip, port, flag)
+        except:
+            eprint(traceback.format_exc())
+            return None
+        
+        return pckt
+
+    def routePacket(self, packet, rules):
+        """
+        decides on the route that the current rules dictate for the packet
+        """
+        rule_counter = 1
+        for rule in rules:
+            # check rule mask vs packet ip
+            ip = IPHelper.ipToLong(packet._ip)
+            if rule._raw_ip == '*' or (rule._ip_mask_val & ip == rule._ip_mask_val):
+                if rule._direction == packet._direction:
+                    for p in rule._ports:
+                        if p == packet._port or p == '*':
+                            if rule._flag is None:
+                                #packet is non-established connection
+                                if rule._action == 'accept':
+                                    return (True, rule_counter, rule)
+                                elif rule._action == 'deny':
+                                    return (False, rule_counter, rule)
+                            #may not need to return rules
+                            elif rule._flag == 'established' and packet._flag == '1':
+                                #packet is established connection
+                                if rule._action == 'accept':
+                                    return (True, rule_counter, rule)
+                                elif rule._action == 'deny':
+                                    return (False, rule_counter, rule)
+                            else:
+                                pass
+                        else:
+                            pass
+                else:
+                    pass
+            else:
+                pass
+
+            rule_counter += 1
+        return (False, 0, None)
         
 
 class Rule:
@@ -100,14 +150,14 @@ class Rule:
             real_ip = ip_parts[0]
             try:
                 subnet_mask_raw = int(subnet_mask)
-                ipLong = RuleManager.ipToLong(real_ip)
+                ipLong = IPHelper.ipToLong(real_ip)
 
                 subnet_mask = 1 << (subnet_mask_raw - 1)
                 subnet_mask = subnet_mask | (subnet_mask - 1)
                 subnet_mask = subnet_mask << (32 - subnet_mask_raw)
 
                 self._ip_mask_val = subnet_mask & ipLong
-                self._ip_mask_str = RuleManager.longToIp(self._ip_mask_val)
+                self._ip_mask_str = IPHelper.longToIp(self._ip_mask_val)
             except ValueError:
                 #poorly formed subnet mask
                 raise RuleException("Malformed subnet mask")
@@ -139,65 +189,34 @@ class RuleException(Exception):
 
     def __str__(self):
         return repr(self._message)
-
     
-def checkPacket(self, packet):
-    """ 
-    checks to see if a packet conforms to any rules and returns the rule number.
-    returns -1 if no rule found
-    """
-    
-    pcktParts = packet.split()
-    
-    # needs exactly 4 parts
-    if len(pcktParts) != 4:
-        return None
-    
-    direction = pcktParts[0]
-    ip = pcktParts[1]
-    port = pcktParts[2]
-    flag = pcktParts[3]
-    
-    try:
-        pckt = Packet(direction, ip, port, flag)
-        print("Made a packet!")
-    except:
-        return -1
-    
-    return pckt
 
 class Packet:
     """  """
     def __init__(self, direction, ip, port, flag):
         self._direction = direction
-
         if self._direction != 'in' and self._direction != 'out':
             raise PacketException("Packet object field 'direction' must be either 'in' or 'out'")
 
         try:
-            ipLong = RuleManager.ipToLong(ip)
+            ipLong = IPHelper.ipToLong(ip)
         except:
             raise PacketException("Packet object field 'ip' must be a valid IPv4 address")
-        finally:
-            self._ip = ip
-
-        if port >=0 and port < 65536:
+        self._ip = ip
+            
+        if int(port) >=0 and int(port) < 65536:
             self._port = port
         else:
             raise PacketException("Packet object field 'port' must be between 0 and 65535")
 
-        if not (flag == 1 or flag == 0):
+        if not (flag == '1' or flag == '0'):
             raise PacketException("Packet object field 'flag' must be either '0' or '1'")
         self._flag = flag
-
-
-        def __str__(self):
-            my_str = "direction: " + self._direction + "\n"
-            my_str += "ip: " + self._raw_ip + "\n"
-            my_str += "port: " + str(self._port) + "\n"
-            my_str += "flag: " + self._flag + "\n"
-
-            return str(my_str)
+        
+    def __str__(self):
+        my_str = self._direction + " " + self._ip + " " + \
+                 str(self._port) + " " + self._flag
+        return str(my_str)
 
 class PacketException(Exception):
     def __init__(self, msg):
@@ -206,6 +225,19 @@ class PacketException(Exception):
     def __str__(self):
         return repr(self._message)
 
+class IPHelper:
+    @staticmethod
+    def ipToLong(ip):
+        """ takes in a string format ip and converts it to a long int """
+        packedIP = socket.inet_aton(ip)
+        return struct.unpack("!L", packedIP)[0]
+
+    @staticmethod
+    def longToIp(longIp):
+        """ takes in a long format ip and converts it to a string """
+        stringIp = socket.inet_ntoa(struct.pack("!L", longIp))
+        return stringIp
+    
 def main(args = None):
     if len(sys.argv) < 2:
         eprint("Invalid usage. Usage: ")
@@ -216,11 +248,22 @@ def main(args = None):
     rule_manager = RuleManager()
     rule_manager.parseRules(rules)
 
-    #rule_manager.pckt
-    
     if sys.stdin.isatty():
         eprint("No input file received.")
         return
+    
+    line = sys.stdin.readline()
+    while line != '':
+        pckt = rule_manager.parsePacket(line)
+        if pckt is not None:
+            (status, rno, rule) = rule_manager.routePacket(pckt, rule_manager._rules)
+            if rno == 0:
+                print("drop() {0}".format(pckt.__str__()))
+            else:
+                print("{0}({1}) {2}".format(rule._action, rno, pckt.__str__()))
+        else:
+            eprint("Corrupt Packet")
+        line = sys.stdin.readline()
     
 if __name__ == "__main__":
     main()
